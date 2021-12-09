5454bc72c885ba300230cc2d97841f26b44163a1
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-12-08 17:28:51 -0800

    added images and css styling to app.css

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default function List({ pokemon }) {
   return (
-    <div>
+    <div className="poke-container">
       {pokemon.map((p) => (
-        <p key={p.id}>{p.pokemon}</p>
+        <p key={p.id}>
+          {p.pokemon} <img src={p.url_image}></img>
+        </p>
       ))}
     </div>
   );
